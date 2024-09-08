0c777fc591fd1952756a635cdf719a737107a50a
mothukuru saikiran <obscuresai.com>
2024-09-08 13:35:12 +0530

    message

--- 01-vpc/parameters.tf
+++ 01-vpc/parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "vpc_id" {
-    name = "/${var.project_name}/${var.environment}/vpc_id"
+    name = "/${var.project_name}/${var.environment}/vpc_id"  # It is name which is nothing but key and value will also be there
     type = "String"
     value = module.roboshop.vpc_id  # Ouput --> It should be there inside the module outputs,module should output then only is is used
 }
