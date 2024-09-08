02a34c6ec43f10173d2946e57982b2754c917901
mothukuru saikiran <obscuresai.com>
2024-09-08 14:33:11 +0530

    message

--- 04-ec2/locals.tf
+++ 04-ec2/locals.tf
@@ -1,6 +1,8 @@
 locals {
   ec2_name           = "${var.project_name}-${var.environment}"
-  database_subnet_id = element(split(",", data.aws_ssm_parameter.database_subnet_ids.value), 0)
+  database_subnet_id = element(split(",", data.aws_ssm_parameter.database_subnet_ids.value), 0) 
   private_subnet_id = element(split(",", data.aws_ssm_parameter.private_subnet_ids.value), 0)
   public_subnet_id = element(split(",", data.aws_ssm_parameter.public_subnet_ids.value), 0)
 }
+
+# Here converting the string into a list by using "," and then spliting and then taking the first element by using element function 
