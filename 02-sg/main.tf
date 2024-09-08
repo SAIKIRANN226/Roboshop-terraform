fc9f0f7d9b939302f5a658cb65e05bc7a1b802d4
mothukuru saikiran <obscuresai.com>
2024-09-08 14:16:58 +0530

    message

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -129,7 +129,7 @@ resource "aws_security_group_rule" "mongodb_vpn" {
 }
 
 #mongodb accepting connections from catalogue instance
-resource "aws_security_group_rule" "mongodb_catalogue" {  # It is convention like mongodb aacepting connections from catalogue
+resource "aws_security_group_rule" "mongodb_catalogue" {  # It is convention like mongodb aacepting connections from catalogue,similarly same for other rules also
   source_security_group_id = module.catalogue.sg_id
   type                     = "ingress"
   from_port                = 27017
@@ -166,7 +166,7 @@ resource "aws_security_group_rule" "redis_cart" {  # It is convention like redis
 }
 
 
-resource "aws_security_group_rule" "mysql_vpn" {   
+resource "aws_security_group_rule" "mysql_vpn" {    
   source_security_group_id = module.vpn.sg_id
   type                     = "ingress"
   from_port                = 22
