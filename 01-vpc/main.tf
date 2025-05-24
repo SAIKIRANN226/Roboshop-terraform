22c6431d300a3c112c7dd9121c23f78caa0cdfdc
mothukuru saikiran <obscuresai.com>
2025-05-24 19:50:10 +0530

    Updated the code

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -1,21 +1,23 @@
 module "roboshop" {
-  #source = "../terraform-aws-vpc"
+  # source = "../terraform-aws-vpc"
   source = "git::https://github.com/daws-76s/terraform-aws-vpc.git?ref=main"
   project_name = var.project_name
   environment = var.environment
   common_tags = var.common_tags
   vpc_tags = var.vpc_tags
 
-  # public subnet
+  # Public subnet
   public_subnets_cidr = var.public_subnets_cidr
 
-  # private subnet
+  # Private subnet
   private_subnets_cidr = var.private_subnets_cidr
 
-  # database subnet
+  # Database subnet
   database_subnets_cidr = var.database_subnets_cidr
 
-  #peering
+  # Peering
   is_peering_required = var.is_peering_required
-
 }
+
+
+# The above code is nothing but whatever there in the vpc-test code same code we put here.
