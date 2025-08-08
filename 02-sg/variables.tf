d4a88894ed175e94c5224c06cce909babbba8b17
mothukuru saikiran <obscuresai.com>
2025-08-08 21:44:06 +0530

    testing

--- 02-sg/variables.tf
+++ 02-sg/variables.tf
@@ -21,14 +21,14 @@ variable "mongodb_sg_ingress_rules" {
   default = [
     {
       description = "Allow port 80"
-      from_port   = 80 # 0 means all ports
+      from_port   = 80 
       to_port     = 80
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     },
     {
       description = "Allow port 443"
-      from_port   = 443 # 0 means all ports
+      from_port   = 443 
       to_port     = 443
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
